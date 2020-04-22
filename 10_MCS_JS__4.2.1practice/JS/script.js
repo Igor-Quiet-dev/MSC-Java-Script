335c2ca35da9efb2b5cbc2a5d93df5a0f8e25ad0
quiet Igor <46747729+greeneefff@users.noreply.github.com>
2020-04-22 18:22:20 +0300

    Practice 4

---
+++ 10_MCS_JS__4.2.1practice/JS/script.js
@@ -0,0 +1,14 @@
+console.log('You are at' +window.cocation);
+
+let answer = prompt("Введите число");
+answer = parseInt(answer);
+
+if(answer>0) {
+   console.log(answer);
+              }    
+else if (answer<=0){
+    console.log(0);
+}else {
+    console.log("Try first");
+}
+
