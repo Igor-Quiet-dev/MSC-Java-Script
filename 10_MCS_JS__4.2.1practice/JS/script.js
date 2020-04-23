95cb0a2e09c2575d1cbf5984943d3a7689023f5a
quiet Igor <46747729+greeneefff@users.noreply.github.com>
2020-04-23 13:54:37 +0300

    Update script.js

--- 10_MCS_JS__4.2.1practice/JS/script.js
+++ 10_MCS_JS__4.2.1practice/JS/script.js
@@ -3,12 +3,13 @@ console.log('You are at' +window.cocation);
 let answer = prompt("Введите число");
 answer = parseInt(answer);
 
-if(answer>0) {
-   console.log(answer);
+if(answer<0) {
+   console.log(answer*(-1));
               }    
-else if (answer<=0){
-    console.log(0);
-}else {
+else if (answer>0){
+    console.log(answer*(-1));
+}
+else{ 
     console.log("Try first");
 }
 
