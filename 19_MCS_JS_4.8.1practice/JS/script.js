cc5eba3d6aefcb16afd09a3106865743a706516a
quiet Igor <46747729+greeneefff@users.noreply.github.com>
2020-06-10 15:31:58 +0300

    Update script.js

--- 19_MCS_JS_4.8.1practice/JS/script.js
+++ 19_MCS_JS_4.8.1practice/JS/script.js
@@ -1,25 +1,24 @@
-console.log('You are at' +window.location);
+console.log("You are at" + window.location);
 
 let a;
 let b;
 let c;
 let d;
 let e;
-let f;
 
-function haveEnough(
-    a = (parseInt(prompt('сколько у вас с собой есть денег '))),
-    b = (parseInt(prompt('сколько вы купили яблок'))), 
-    c = (parseInt(prompt('сколько вы купили батонов хлеба'))), 
-    d = (parseInt(prompt('сколько стоит одно яблоко'))),
-    e = (parseInt(prompt('сколько стоит один батон хлеба'))),
-    f = b*d+c*e
-    ) 
-    {
-    return (a>=f) ? document.body.innerHTML =  'Вам хватает денег на покупки': document.body.innerHTML =  'Вам не хватает денег';
+function haveEnough(a, b, c, d, e) {
+  var f = b * d + c * e;
+  return a >= f
+    ? (document.body.innerHTML = "Вам хватает денег на покупки")
+    : (document.body.innerHTML = "Вам не хватает денег");
 }
 
-console.log(haveEnough());
-       
-
-
+console.log(
+  haveEnough(
+    parseInt(prompt("сколько у вас с собой есть денег ")),
+    parseInt(prompt("сколько вы купили яблок")),
+    parseInt(prompt("сколько вы купили батонов хлеба")),
+    parseInt(prompt("сколько стоит одно яблоко")),
+    parseInt(prompt("сколько стоит один батон хлеба"))
+  )
+);
